a90646357ce481e33f52e7d65ada47d075b49794
落合　涼介 <OIBC34081@ojs.ac.jp>
2023-04-19 10:09:58 +0900

    新規作成

---
+++ sample/BallApp/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BallApp {
+    class Program :Form {
+
+        private Timer moveTimer;  //タイマー用
+        private SoccerBall soccerball;
+        private PictureBox pb;   //画像を表示するコントロール
+
+        static void Main(string[] args) {
+
+            Application.Run(new Program());
+
+        }
+        public Program() {
+            this.Size = new Size(800, 600);
+            this.BackColor = Color.LawnGreen;
+            this.Text = "BallGame";
+
+            //ボールインスタンス生成
+            soccerball = new SoccerBall();
+            pb = new PictureBox();//画像を表示するコントロール
+            pb.Image = soccerball.Image;
+            pb.Location = new Point((int)soccerball.PosX, (int)soccerball.PosY);//画像の位置
+            pb.Size = new Size(50, 50);   //画像の表示サイズ
+            pb.SizeMode = PictureBoxSizeMode.StretchImage;   //画像の表示モード
+            pb.Parent = this;
+
+            moveTimer = new Timer();
+            moveTimer.Interval = 1; //タイマーのインターバル
+            moveTimer.Start();   //タイマースタート
+            moveTimer.Tick += MoveTimer_Tick;
+            
+        }
+
+        private void MoveTimer_Tick(object sender, EventArgs e) {
+            soccerball.Move();  //移動
+            pb.Location = new Point((int)soccerball.PosX, (int)soccerball.PosY);//画像の位置
+        }
+    }
+}
---
+++ sample/BallApp/SoccerBall.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BallApp {
+    class SoccerBall {
+        //フィールド
+        private Image image;   //画像データ
+
+        private double posX;    //x座標
+        private double posY;    //y座標
+
+        private double moveX = 10;  //移動量
+        private double moveY = 10;  //移動量
+
+        //コンストラクタ
+        public SoccerBall() {
+            Image = Image.FromFile(@"pic\soccer_ball.png");
+            posX = 0.0;
+            PosY = 0.0;
+
+        }
+        //プロパティ
+        public double PosX { get => posX; set => posX = value; }
+        public double PosY { get => posY; set => posY = value; }
+        public Image Image { get => image; set => image = value; }
+
+        //メソッド
+        public void Move() {
+
+            posX += moveX;
+            posY += moveY;
+
+            if (posX > 740 || posX < 0)
+            {
+                moveX *= -1;
+            }
+            else if (posY > 520 || posY < 0)
+            {
+                moveY *= -1;
+            }
+        }
+    }
+}
