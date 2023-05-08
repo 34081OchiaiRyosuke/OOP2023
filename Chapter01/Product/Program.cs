05947a82031e4576327009350a7d0b67b68f8dc7
落合　涼介 <OIBC34081@ojs.ac.jp>
2023-05-08 11:50:41 +0900

    0508演習1

--- Chapter01/Product/Program.cs
+++ Chapter01/Product/Program.cs
@@ -5,14 +5,31 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace ProductSample {
-    //商品クラス
+
     class Program {
         static void Main(string[] args) {
+
+            #region P25のサンプルプログラム
             //インスタンスの生成
-            Product karinto = new Product(123, "かりんとう", 180);
-            Product daifukumoti = new Product(235, "大福もち", 160);
-            Console.WriteLine("税込み価格" + karinto.GetPriceIncludingTax());
-            Console.WriteLine("税込み価格" + daifukumoti.GetPriceIncludingTax());
+            //Product karinto = new Product(123, "かりんとう", 180);
+            //Product daifukumoti = new Product(235, "大福もち", 160);
+            //Console.WriteLine("税込み価格" + karinto.GetPriceIncludingTax());
+            //Console.WriteLine("税込み価格" + daifukumoti.GetPriceIncludingTax());
+            #endregion
+
+            
+            DateTime date = DateTime.Today;
+
+            //10日後を求める
+            DateTime daysAfter10 = date.AddDays(10);
+
+            //10日前を求める
+            DateTime daysBefore10 = date.AddDays(-10);
+
+
+            Console.WriteLine("今日の日付:" + date);
+            Console.WriteLine("今日の10日後は" + daysAfter10.ToString(@"yyyy\/MM\/dd") + "日です");
+            Console.WriteLine("今日の10日後は" + daysBefore10.ToString(@"yyyy\/MM\/dd") + "日です");
 
 
         }
