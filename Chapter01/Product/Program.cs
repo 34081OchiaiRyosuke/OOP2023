362a02cdcc6cf9616224cc1987926366e805d320
落合　涼介 <OIBC34081@ojs.ac.jp>
2023-05-08 10:54:02 +0900

    P26まで

---
+++ Chapter01/Product/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductSample {
+    //商品クラス
+    class Program {
+        static void Main(string[] args) {
+            //インスタンスの生成
+            Product karinto = new Product(123, "かりんとう", 180);
+            Product daifukumoti = new Product(235, "大福もち", 160);
+            Console.WriteLine("税込み価格" + karinto.GetPriceIncludingTax());
+            Console.WriteLine("税込み価格" + daifukumoti.GetPriceIncludingTax());
+
+
+        }
+    }
+}
