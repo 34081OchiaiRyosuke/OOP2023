f550298716af9d33189b3a1056cd783ebb63bb11
落合　涼介 <OIBC34081@ojs.ac.jp>
2023-05-16 10:51:57 +0900

    コンストラクタ修正

--- Chapter02/売上集計プログラム/SalesCalculator/Program.cs
+++ Chapter02/売上集計プログラム/SalesCalculator/Program.cs
@@ -8,7 +8,7 @@ using System.Threading.Tasks;
 namespace SalesCalculator {
     class Program {
         static void Main(string[] args) {
-            SalesCounter sales = new SalesCounter(SalesCounter.ReadSales(@"data\sales.csv"));
+            SalesCounter sales = new SalesCounter(@"data\sales.csv");
             Dictionary<string, int> amountParStore = sales.GetParStoreSales();
             foreach (KeyValuePair<string, int> obj in amountParStore) {
                 Console.WriteLine("{0} {1:C}", obj.Key, obj.Value);
