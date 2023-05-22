23a9461f65b2af7d6149a22918df6f3a581c55a8
落合　涼介 <OIBC34081@ojs.ac.jp>
2023-05-22 10:02:42 +0900

    P79 2.3 商品カテゴリーへ変更

--- Chapter02/演習問題/Exercise03/Exercise03/Program.cs
+++ Chapter02/演習問題/Exercise03/Exercise03/Program.cs
@@ -7,7 +7,12 @@ using System.Threading.Tasks;
 namespace Exercise03 {
     class Program {
         static void Main(string[] args) {
-
+           
+            var sales = new SalesCounter(@"data\sales.csv");
+            var amountParStore = sales.GetParStoreSales();
+            foreach (var obj in amountParStore) {
+                Console.WriteLine("{0} {1:C}", obj.Key, obj.Value);
+            }
         }
     }
 }
