6a2ce5d33a05fd74699b2c49a155d8981915686c
落合　涼介 <OIBC34081@ojs.ac.jp>
2023-05-24 11:51:04 +0900

    P100 演習問題 途中

---
+++ Chapter03/演習問題/Exercise02/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise02 {
+    class Program {
+        static void Main(string[] args) {
+            var names = new List<string> {
+                 "Tokyo", "New Delhi", "Bangkok", "London", "Paris", "Berlin", "Canberra", "Hong Kong",
+            };
+            Exercise2_1(names);
+            Console.WriteLine();
+            Exercise2_2(names);
+            Console.WriteLine();
+            Exercise2_3(names);
+            Console.WriteLine();
+            Exercise2_4(names);
+        }
+
+        private static void Exercise2_1(List<string> names) {
+            Console.Write("都市名を入力、空白で終了:");
+            do {
+                var city = Console.ReadLine();
+                if (string.IsNullOrEmpty(city)){
+                    break;
+                }
+                var list = names.FindIndex(n => n == city);
+                Console.WriteLine(list);
+            } while (true);
+             
+        }
+
+        private static void Exercise2_2(List<string> names) {
+            var city = names.Count(n => n.Contains("o"));
+            Console.WriteLine(city);
+        }
+
+        private static void Exercise2_3(List<string> names) {
+            names.Where(n => n.Contains("o")).ToList().ForEach(Console.WriteLine);
+        }
+
+        private static void Exercise2_4(List<string> names) {
+            
+        }
+    }
+}
