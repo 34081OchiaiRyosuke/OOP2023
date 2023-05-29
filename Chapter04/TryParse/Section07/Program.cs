1d9251d8573db3a191ab7f4048a53b4ef371b57f
落合　涼介 <OIBC34081@ojs.ac.jp>
2023-05-29 12:09:51 +0900

    TryParse

---
+++ Chapter04/TryParse/Section07/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Section07 {
+    class Program {
+        static void Main(string[] args) {
+
+            var str = "123";
+
+            int heigth;
+            if (int.TryParse(str,out heigth)) {
+                //heigthには変換された値が入っている
+                Console.WriteLine(heigth);
+            }
+            else {
+                //変換が失敗した
+                Console.WriteLine("変換できません");
+            }
+        }
+    }
+}
