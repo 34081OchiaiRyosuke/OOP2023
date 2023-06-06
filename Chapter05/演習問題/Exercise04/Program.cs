ad12668a6192d2653b539c28ab6611cc5da92476
落合　涼介 <OIBC34081@ojs.ac.jp>
2023-06-06 10:31:09 +0900

    P159 5.4 完成

--- Chapter05/演習問題/Exercise04/Program.cs
+++ Chapter05/演習問題/Exercise04/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -7,6 +8,8 @@ using System.Threading.Tasks;
 namespace Exercise04 {
     class Program {
         static void Main(string[] args) {
+            Stopwatch sw = new Stopwatch(); //タイマーインスタンス生成
+            sw.Start(); //タイマースタート
 #if NonArray
             var line = "Novelist=谷崎潤一郎;BestWork=春琴抄;Born=1886";
 #elif StringArray
@@ -14,7 +17,16 @@ namespace Exercise04 {
             "Novelist=谷崎潤一郎;BestWork=春琴抄;Born=1886",
             "Novelist=夏目礎石;BestWork=坊ちゃん;Born=1867",
             "Novelist=太宰治;BestWork=人間失格;Born=1909",
-            "Novelist=宮沢賢治;BestWork=銀河鉄道の夜;Born=1896"
+            "Novelist=宮沢賢治;BestWork=銀河鉄道の夜;Born=1896",
+            "Novelist=谷崎潤一郎;BestWork=春琴抄;Born=1886",
+            "Novelist=夏目礎石;BestWork=坊ちゃん;Born=1867",
+            "Novelist=太宰治;BestWork=人間失格;Born=1909",
+            "Novelist=宮沢賢治;BestWork=銀河鉄道の夜;Born=1896",
+            "Novelist=谷崎潤一郎;BestWork=春琴抄;Born=1886",
+            "Novelist=夏目礎石;BestWork=坊ちゃん;Born=1867",
+            "Novelist=太宰治;BestWork=人間失格;Born=1909",
+            "Novelist=宮沢賢治;BestWork=銀河鉄道の夜;Born=1896",
+            
             };
 #endif
 
@@ -25,10 +37,16 @@ namespace Exercise04 {
             Console.WriteLine("代表作：" + words[3]);
             Console.WriteLine("誕生年：" + words[5]);
 #elif StringArray
-
+            for (int i = 0; i < lines.Length; i++) {
+                var words = lines[i].Split(';', '=');
+                Console.WriteLine("作家　：" + words[1]);
+                Console.WriteLine("代表作：" + words[3]);
+                Console.WriteLine("誕生年：" + words[5]);
+            }
 
 
 #endif
+            Console.WriteLine("実行時間 = {0}", sw.Elapsed.ToString(@"ss\.fffff"));//時間表示
         }
     }
 }
