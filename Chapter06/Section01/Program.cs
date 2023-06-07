3580e254b1445529c41bb795d2a404ca6f7ea1c9
落合　涼介 <OIBC34081@ojs.ac.jp>
2023-06-07 09:59:51 +0900

    オブジェクトのLINQ

---
+++ Chapter06/Section01/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Section01 {
+    class Program {
+        static void Main(string[] args) {
+            var number = new List<int> { 9, 7, 5, 4, 2, 5, 4, 0, 4, 1, 0, 4 };
+            Console.WriteLine(number.Average());
+
+            var books = Books.GetBooks();
+
+            //500円以上の本のタイトルをすべて出力
+            var booksObj = books.Where(x => x.Title.Contains("物語")).OrderByDescending(x => x.Price).Average(x => x.Pages);
+            Console.WriteLine("平均{0}ページ",booksObj);
+
+            var longTitleBooks = books.OrderByDescending(x => x.Title.Length);
+
+            foreach (var book in longTitleBooks) {
+                Console.WriteLine("{0} {1}", book.Title, book.Price);
+            }
+
+
+        }
+    }
+}
