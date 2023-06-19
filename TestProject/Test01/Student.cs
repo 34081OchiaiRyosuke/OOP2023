5d03c3d7c8495496e1b640d4379fef481f2a16e4
落合　涼介 <OIBC34081@ojs.ac.jp>
2023-06-19 15:02:19 +0900

    設問１_Studentクラス完成

--- TestProject/Test01/Student.cs
+++ TestProject/Test01/Student.cs
@@ -1,7 +1,11 @@
 ﻿namespace Test01 {
     class Student {
+        string Name;//学生の名前
+        string Subject;//科目名
+        int Score; //点数
 
-
-
+        public string Name1 { get => Name; set => Name = value; }
+        public string Subject1 { get => Subject; set => Subject = value; }
+        public int Score1 { get => Score; set => Score = value; }
     }
 }
