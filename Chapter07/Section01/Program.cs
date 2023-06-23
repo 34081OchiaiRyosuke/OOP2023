7b5b8ce98648eae5b0c74878918bf11e0a5e7a0a
落合　涼介 <OIBC34081@ojs.ac.jp>
2023-06-23 10:59:30 +0900

    県庁所在地の登録　人口追加

--- Chapter07/Section01/Program.cs
+++ Chapter07/Section01/Program.cs
@@ -7,19 +7,22 @@ using System.Threading.Tasks;
 namespace Section01 {
     class Program {
         static void Main(string[] args) {
-            var ken = new Dictionary<string, string>() {
+            var ken = new Dictionary<string, CityInfo>() {
             };
             Console.Write("県名:");
             var kenmei = Console.ReadLine();
 
             while (kenmei != "999") {
+                var cityinfo = new CityInfo();
                 Console.Write("所在地:");
-                var syozaiti = Console.ReadLine();
-                ken[kenmei] = syozaiti;
+                cityinfo.City = Console.ReadLine();
+                Console.Write("人口を入力");
+                cityinfo.Population = int.Parse(Console.ReadLine());
+                ken[kenmei] = new CityInfo { City = cityinfo.City, Population = cityinfo.Population };
                 Console.Write("県名:");
                 kenmei = Console.ReadLine();
                 if (ken.ContainsKey(kenmei)) {
-                    Console.WriteLine("上書きしますか yes/no");
+                    Console.WriteLine("重複しています、上書きしますか yes/no");
                     var uwagaki = Console.ReadLine();
                     if(uwagaki == "no") {
                         Console.Write("県名:");
@@ -31,7 +34,7 @@ namespace Section01 {
             var hyoji = Console.ReadLine();
             if (hyoji == "1") {
                 foreach (var item in ken) {
-                    Console.WriteLine("{0}({1})",item.Key, item.Value);
+                    Console.WriteLine("{0}({1}[{2}])",item.Key, item.Value.City,item.Value.Population);
                 }
             }
             else {
@@ -42,7 +45,7 @@ namespace Section01 {
         }
     }
     class CityInfo {
-        public string City { get; set; }
-        public int Population { get; set; }
+        public string City { get; set; }　　　//都市
+        public int Population { get; set; }   //人口
     }
 }
