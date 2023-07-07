d8e6c6ee46ebe9e01d38d5ca3d10cf6aadb66e32
落合　涼介 <OIBC34081@ojs.ac.jp>
2023-07-07 12:06:47 +0900

    0707 途中経過

--- FormApps/CarReportSystem/Form1.cs
+++ FormApps/CarReportSystem/Form1.cs
@@ -10,8 +10,26 @@ using System.Windows.Forms;
 
 namespace CarReportSystem {
     public partial class Form1 : Form {
+        //管理用データ
+        IEnumerable<CarReport> CarReports = new BindingList<CarReport>();
+
         public Form1() {
             InitializeComponent();
+            dgvCarReports.DataSource = CarReports;
+        }
+
+        //追加ボタンがクリックされた時のイベントハンドラー
+        private void btAddReport_Click(object sender, EventArgs e) {
+            var car  = new CarReport {          //Saleインスタンス生成
+                Date = dtpDate.Value,
+                Author = cbAuthor.Text,
+                //Maker = gbMaker,
+                //CarName = ,
+                //Report = ,
+                //CarImage = ,
+            };
+
+
         }
     }
 }
