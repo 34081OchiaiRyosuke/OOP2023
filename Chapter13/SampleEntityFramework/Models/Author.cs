bc078c28dc7c5b108a0e9f94a4a7320be1764265
落合　涼介 <OIBC34081@ojs.ac.jp>
2023-09-13 11:09:26 +0900

    データ注釈

--- Chapter13/SampleEntityFramework/Models/Author.cs
+++ Chapter13/SampleEntityFramework/Models/Author.cs
@@ -1,12 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace SampleEntityFramework.Models {
     public class Author {
         public int Id { get; set; }
+        [MaxLength(30)]
+        [Required]
         public string Name { get; set; }
         public DateTime Birthday { get; set; }
         public string Gender { get; set; }
